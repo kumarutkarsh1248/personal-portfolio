97005b27b824b99ce24e9fae7623e0952b9b3df3
kumarutkarsh1248 <kumar.utkarsh.civ22@iitbhu.ac.in>
2023-06-11 13:56:43 +0530

    personal portfolio at the end of week1

--- canvas_script.js
+++ canvas_script.js
@@ -4,7 +4,9 @@ let ctx = canvas.getContext("2d");
 window.addEventListener("resize",function()
 {
     canvas.width=window.innerWidth
-    canvas.height=window.innerHeight
+    // canvas.height=window.innerHeight
+    canvas.height = 700;
+
     ctx=canvas.getContext("2d")
 })
 
@@ -16,9 +18,9 @@ let particleArray = [];
 
 class Particle{
     constructor(xCor, yCor, size, speedx, speedy, color1){
-        this.x = xCor;
-        this.y = yCor;
-        this.size = size;
+        this.x = (window.innerWidth/100)*xCor;
+        this.y = 7*yCor;
+        this.size = 7*size;
         this.speedx = speedx;
         this.speedy = speedy;
         this.color1 = color1;
@@ -41,7 +43,7 @@ class Particle{
     draw(){
         this.gradient = ctx.createRadialGradient(this.x+this.size/4, this.y-this.size/3, this.size/10000, this.x, this.y, this.size)
         this.gradient.addColorStop(0, "#5967be");
-        this.gradient.addColorStop(1, "#13175e")
+        this.gradient.addColorStop(1, "#0a144f")
         
         ctx.beginPath();
 
@@ -54,13 +56,13 @@ class Particle{
 } 
 
 
-const particle1 = new Particle(500, 200, 10, -0.5, 1.5, "white");
-const particle2 = new Particle(500, 200, 20, 1, 0.4, "white");
-const particle3 = new Particle(400, 500, 30, 1, -1, "white");
-const particle4 = new Particle(200, 200, 40, -1, -2, "white");
-const particle5 = new Particle(200, 200, 60, -0.2, 1, "white");
-const particle6 = new Particle(200, 500, 80, 1, 0.5, "white");
-const particle7 = new Particle(600, 600, 100, -0.5, -1, "white");
+const particle1 = new Particle(90, 90, 2, -0.5, 1, "white");
+const particle2 = new Particle(50, 50, 4, 1, 0.4, "white");
+const particle3 = new Particle(80, 30, 6, 1, -0.8, "white");
+const particle4 = new Particle(10, 90, 8, -0.8, -0.9, "white");
+const particle5 = new Particle(70, 20, 10, -0.2, 0.8, "white");
+const particle6 = new Particle(20, 30, 14, 0.6, -0.5, "white");
+const particle7 = new Particle(60, 60, 17, -0.5, 0.8, "white");
 
 particleArray.push(particle1);
 particleArray.push(particle2);
--- celebrate.js
+++ celebrate.js
@@ -19,20 +19,28 @@ let area_computed = window.getComputedStyle(area_element);
 let ball = document.querySelector(".ball");
 let ball_information = ball.getBoundingClientRect();
 let ball_computed = window.getComputedStyle(ball);
-console.log(ball_information)
+// console.log(ball_information)
+let cel_img = document.querySelector("#area img");
+let cel_img_info = cel_img.getBoundingClientRect();
+
+document.querySelector(" .celebration_img img").left = cel_img_info.left;
+document.querySelector(" .celebration_img img").top = cel_img_info.top;
+
+console.log(`x:${cel_img_info.left}, y:${cel_img_info.top}`)
+
 
 function addBall_updateMouse(e){
-    mouse.x = e.x - area_information.left;
-    mouse.y = e.y - area_information.top;
-    for(let i=0; i<100; i++){
+    // mouse.x = e.x - area_information.left;
+    // mouse.y = e.y - area_information.top;
+    for(let i=0; i<60; i++){
         let random_angle = 190+Math.random()*50;
-        let random_speed = 5+Math.random()*10;
-        ball_array.push(new Ball(mouse.x, mouse.y, 60, random_speed, random_angle, 0.2))
-
+        let random_speed = 10+Math.random()*10;
+        ball_array.push(new Ball(cel_img_info.left+25,cel_img_info.top+25, 60, random_speed, random_angle, 0.25))
+        // mouse.x, mouse.y
     }
 }
 function move(){
-    console.log(`mouse x${mouse.x}`)
+    // console.log(`mouse x${mouse.x}`)
     let area_width = +(area_computed.width).slice(-area_computed.width.length,-2);
     let area_height = +(area_computed.height).slice(-area_computed.height.length,-2);
     // console.log(area_height)
@@ -89,19 +97,19 @@ class Ball{
     }
 }
 
-document.querySelector("#area").addEventListener("click", addBall_updateMouse);
+document.querySelector("#area img").addEventListener("click", addBall_updateMouse);
 window.setInterval(move, 10)
 
+document.querySelector(".celebration_img img").addEventListener("click", ()=>{
+    addBall_updateMouse();
+    document.querySelector("#area").style.zIndex = "3";
+    document.querySelector(".celebration_img").style.zIndex = "-1";
 
-// document.querySelector('#area').addEventListener('wheel', preventScroll, {passive: false});
-
-function preventScroll(e){
-    e.preventDefault();
-    e.stopPropagation();
-
-    return false;
-}
-
-document.querySelector('#area').forEach(function(item){
-    item.addEventListener('wheel', preventScroll, {passive: false});
+    setTimeout(()=>{
+        document.querySelector("#area").style.zIndex = "-1";
+        document.querySelector(".celebration_img").style.zIndex = "auto";
+    }, 5000)
 });
+
+
+console.log(document.querySelector("#area_absolute").style)
